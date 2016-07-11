2d604439607bd5650476e32aa59ee818acc92062
Scott Vrable <scott@scottvrable.com>
2016-07-10 22:26:12 -0700

    Skeleton stylings of buttons

--- src/components/header.js
+++ src/components/header.js
@@ -7,10 +7,14 @@ class Header extends Component {
 				<div className="col-xs-12">
 					<div className="container">
 						<div className="row">
-							<div className="col-xs-12 col-md-6">
-								<h1 className="text-xs-center text-md-left">
+							<div className="col-xs-12">
+								<h1 className="text-xs-center pull-md-left">
 									The Amazing <span className="featured-animal">Llama</span> Viewer!
 								</h1>
+								<div className="button-group text-xs-center pull-md-right">
+									<button className="btn show-diff-btn text-capitalize">Show me something different</button>
+									<button className="btn show-more-btn text-capitalize">Show me more llamas!</button>
+								</div>
 							</div>
 						</div>
 					</div>
