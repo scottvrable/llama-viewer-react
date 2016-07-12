c5000bd424c0c376085d46bb1ddf1765a6c36d36
Scott Vrable <scott@scottvrable.com>
2016-07-11 22:26:20 -0700

    Add Gallery placeholder and redirect from "/"

---
+++ src/components/gallery.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+
+class Gallery extends Component {
+	render() {
+		return (
+			<div className="gallery">
+				<div className="col-xs-12">
+					<div className="container">
+						<div className="row">
+							<div className="col-xs-12">
+								Gallery
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Gallery;
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from "react";
-import {Route, IndexRoute} from "react-router";
+import {Route, IndexRoute, IndexRedirect} from "react-router";
 
 import App from "./components/app";
+import Gallery from "./components/gallery";
 
 export default (
 	<Route path="/" component={App}>
-
+		<IndexRedirect to="/llama" />
+		<Route path=":animal" component={Gallery} />
 	</Route>
 );
