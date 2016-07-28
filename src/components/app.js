67afb7db71a0d6cbb09aafc483a5222ebc430ca3
Scott Vrable <scott@scottvrable.com>
2016-07-27 20:17:00 -0700

    Add footer

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Header from "./header";
+import Footer from "./footer";
 
 export default (props) => {
 	return (
 		<div>
 			<Header />
 			{props.children}
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default () => {
+	return (
+		<footer className="row">
+			<div className="col-xs-12">
+				<p className="text-xs-center">
+					&copy;2016 <a href="http://scottvrable.com" target="_blank"> Scott Vrable.</a> Made with <a href="https://facebook.github.io/react/" target="_blank">React</a> and <a href="http://redux.js.org/index.html" target="_blank">Redux.</a> Powered by <a href="https://www.flickr.com/" target="_blank">Flickr.</a> View the Github repo <a href="https://github.com/scottvrable/llama-viewer-react" target="_blank">here.</a>
+				</p>
+			</div>
+		</footer>
+	);
+};
